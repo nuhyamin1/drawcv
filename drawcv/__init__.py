9eadf2078fbefc51bb594898d6edd7fd9357ea10
Nuh Yamin <nuhyamin31@gmail.com>
2026-09-13 12:44:19 +0700

    chore: bump version to 0.9.0

--- drawcv/__init__.py
+++ drawcv/__init__.py
@@ -131,7 +131,7 @@ from drawcv.core.path_processing import (
     slice_stroke_points,
 )
 
-__version__ = "0.8.0"
+__version__ = "0.9.0"
 
 from drawcv.svg import SVGExporter, SVGExport, SVGFallback
 
